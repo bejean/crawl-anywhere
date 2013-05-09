b8b56f2ddb8768a0718b4026a21de66a77b2f4ff
bejean <dominique.bejean@eolya.fr>
2013-05-10 00:55:06 +0200

    no message

--- java/deprecated/solr/src/main/java/org/apache/lucene/analysis/multilingual/TermLanguageAttribute.java
+++
@@ -1,8 +0,0 @@
-package org.apache.lucene.analysis.multilingual;
-
-import org.apache.lucene.util.Attribute;
-
-public interface TermLanguageAttribute extends Attribute {
-	public void setLanguage(String language);
-	public String getLanguage();
-}

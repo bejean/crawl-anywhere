522be122ba57de05bdb1de25942fae167a59e03a
Dominique Béjean <dominique.bejean@eolya.fr>
2014-04-11 00:28:22 +0200

    Crawler queues in a separate mongodb database

--- java/crawler/src/main/java/fr/eolya/crawler/database/CrawlerDBFactory.java
+++ java/crawler/src/main/java/fr/eolya/crawler/database/CrawlerDBFactory.java
@@ -8,12 +8,12 @@ import fr.eolya.utils.nosql.mongodb.MongoDBConnection;
 
 public class CrawlerDBFactory {
 
-	public static ICrawlerDB getCrawlerDBInstance(String type, IDBConnection con, String dbName) {
+	public static ICrawlerDB getCrawlerDBInstance(String type, IDBConnection con, String dbName, String dbNameQueues) {
 		if (!type.equals(con.getType())) return null;
 
 		if ("mongodb".equals(type)) {
 			try {
-				return new MongoDBCrawlerDB((MongoDBConnection) con, dbName);
+				return new MongoDBCrawlerDB((MongoDBConnection) con, dbName, dbNameQueues);
 			} catch (UnknownHostException e) {
 				e.printStackTrace();
 				return null;
